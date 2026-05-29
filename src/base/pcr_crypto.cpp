12b089bbf81a9c6b782b0a9fc270e00c57755aa6
Entity704 <entitymc@outlook.com>
2026-05-29 17:49:14 +0800

    fix: 修复win重定义

--- src/base/pcr_crypto.cpp
+++ src/base/pcr_crypto.cpp
@@ -7,11 +7,17 @@
 #if defined(__EMSCRIPTEN__)
 #include <emscripten.h>
 #include <stddef.h>
+
 #elif defined(_WIN32)
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef _WIN32_WINNT
 #define _WIN32_WINNT 0x0600
+#endif
 #include <windows.h>
 #include <wincrypt.h>
+
 #else
 #include <fcntl.h>
 #include <sys/random.h>
